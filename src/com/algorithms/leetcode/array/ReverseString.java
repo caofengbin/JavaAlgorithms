ee7c07cfa9386940b3a643ad554d1088800bb033
caofengbin <1050430934@qq.com>
2018-05-26 20:02:32 +0800

    add title ReverseString

--- src/com/algorithms/leetcode/array/ReverseString.java
+++ src/com/algorithms/leetcode/array/ReverseString.java
@@ -6,4 +6,25 @@ package com.algorithms.leetcode.array;
  * 给定一个字符串，将其翻转输出
  */
 public class ReverseString {
+
+    public String reverseString(String s) {
+        char[] charArray = s.toCharArray();
+        int begin = 0;
+        int end = s.length() - 1;
+        while (begin < end) {
+            char temp = charArray[end];
+            charArray[end] = charArray[begin];
+            charArray[begin] = temp;
+            begin++;
+            end--;
+        }
+        return new String(charArray);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ReverseString().reverseString("abc2"));
+        System.out.println(new ReverseString().reverseString("12345"));
+        System.out.println(new ReverseString().reverseString("Acdf789"));
+        System.out.println(new ReverseString().reverseString(""));
+    }
 }
