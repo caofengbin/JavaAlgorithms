639a5cb1c23e21e8cfcce9cb6368d77693975eea
caofengbin <1050430934@qq.com>
2018-05-26 13:57:49 +0800

    implemenst ValidPalindrome

--- src/com/algorithms/leetcode/array/ValidPalindrome.java
+++ src/com/algorithms/leetcode/array/ValidPalindrome.java
@@ -1,30 +1,42 @@
 package com.algorithms.leetcode.array;
 
 /**
- * leetcode 125
- * https://leetcode.com/problems/valid-palindrome/description/
+ * leetcode 125，links:https://leetcode.com/problems/valid-palindrome/description/
+ * 给定一个字符串，判断其是否为回文字符串，注意，需要剔除其中非字母和数字的元素进行判断
  */
 public class ValidPalindrome {
 
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^(a-zA-Z0-9]", "");
         s = s.toLowerCase();
         char[] chars = s.toCharArray();
         int begin = 0;
         int end = s.length() - 1;
         while (begin <= end) {
-            if (Math.abs(chars[begin] - chars[end]) == 0 ||
-                    (Math.abs(chars[begin] - chars[end]) == 32 )) {
+            if (isTargetChar(chars[begin]) && isTargetChar(chars[end])) {
+                if (Math.abs(chars[begin] - chars[end]) == 0 ||
+                        (Math.abs(chars[begin] - chars[end]) == 32)) {
+                    begin++;
+                    end--;
+                } else {
+                    return false;
+                }
+            } else if (!isTargetChar(chars[begin])) {
                 begin++;
+            } else if (!isTargetChar(chars[end])) {
                 end--;
-            } else {
-                return false;
             }
         }
         return true;
     }
 
+    public boolean isTargetChar(char targetChar) {
+        return (targetChar >= 48 && targetChar <= 57) ||
+                (targetChar >= 65 && targetChar <= 90) ||
+                (targetChar >= 97 && targetChar <= 122);
+    }
+
     public static void main(String[] args) {
+        System.out.println(new ValidPalindrome().isPalindrome("? (So mad!)"));
         System.out.println(new ValidPalindrome().isPalindrome("Damosel, a poem? A carol? Or a cameo pale? (So mad!)"));
         System.out.println(new ValidPalindrome().isPalindrome("A man, a plan, a canal: Panama"));
         System.out.println(new ValidPalindrome().isPalindrome("race a car"));
