b665f782e5f49f10dadf24f9c46c215724281f6e
caofengbin <1050430934@qq.com>
2018-05-27 17:46:38 +0800

    implements code RemoveElement

--- src/com/algorithms/leetcode/array/RemoveElement.java
+++ src/com/algorithms/leetcode/array/RemoveElement.java
@@ -7,6 +7,23 @@ package com.algorithms.leetcode.array;
  */
 public class RemoveElement {
 
+    public int removeElement(int[] nums, int val) {
+        // beginPointer代表的是[0,beginPointer)之间的元素全部是非val的
+        int beginPointer = 0;
+        int iteratorPointer = 0;
+        for (; iteratorPointer < nums.length; iteratorPointer++) {
+            if (nums[iteratorPointer] != val) {
+                nums[beginPointer] = nums[iteratorPointer];
+                beginPointer++;
+            }
+        }
+        return beginPointer;
+    }
+
     public static void main(String[] args) {
+        int[] newNums = {3, 2, 2, 3};
+        System.out.println(new RemoveElement().removeElement(newNums, 3));
+        int[] newNums2 = {0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println(new RemoveElement().removeElement(newNums2, 2));
     }
 }
