e0e41282aaddf989fec1d9e9b89c2415ac75764d
caofengbin <1050430934@qq.com>
2018-05-30 00:10:53 +0800

    update read me file

--- README.md
+++ README.md
@@ -35,9 +35,19 @@
 ### (3) 三路快排思想在数组中的使用
 
 #### [75. Sort Colors](https://leetcode.com/problems/sort-colors/description/)
-
+&emsp;&emsp;非常经典的一个题目，可以用计数排序的思想来做。也可以用三路快排的思想处理，将数组划分为0，1，2三个区间，从而进行一遍扫描，遍历过程中不断的修改指针的位置值即可。
 
 #### [88. Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/description/)
 
 
 #### [215. Kth Largest Element in an Array](https://leetcode.com/problems/kth-largest-element-in-an-array/description/)
+
+### (4) 滑动窗口思想在数组中的使用
+
+#### [209. Minimum Size Subarray Sum](https://leetcode.com/problems/minimum-size-subarray-sum/description/)
+
+#### [3.Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/description/)
+
+#### [438. Find All Anagrams in a String](https://leetcode.com/problems/find-all-anagrams-in-a-string/description/)
+
+#### [76. Minimum Window Substring](https://leetcode.com/problems/minimum-window-substring/description/)
--- src/com/algorithms/leetcode/array/MergeSortedArray.java
+++ src/com/algorithms/leetcode/array/MergeSortedArray.java
@@ -1,7 +1,48 @@
 package com.algorithms.leetcode.array;
+
 /**
  * leetcode 88  links:https://leetcode.com/problems/merge-sorted-array/description/
  * 给定两个有序的数组，将他们进行合并
  */
 public class MergeSortedArray {
+    public int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int pointer1 = m - 1;
+        int pointer2 = n - 1;
+        int totalPointer = m + n - 1;
+
+        while (pointer1 >= 0 && pointer2 >= 0) {
+            if (nums1[pointer1] > nums2[pointer2]) {
+                nums1[totalPointer] = nums1[pointer1];
+                pointer1--;
+                totalPointer--;
+            } else {
+                nums1[totalPointer] = nums2[pointer2];
+                pointer2--;
+                totalPointer--;
+            }
+        }
+
+        while (pointer1 >= 0) {
+            nums1[totalPointer--] = nums1[pointer1--];
+        }
+        while (pointer2 >= 0) {
+            nums1[totalPointer--] = nums2[pointer2--];
+        }
+        return nums1;
+
+    }
+
+    private static void printlnArray(int[] nums) {
+        for (int index = 0; index < nums.length; index++) {
+            System.out.print(nums[index] + "    ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        printlnArray(new MergeSortedArray().merge(nums1, 3, nums2, nums2.length));
+    }
+
 }
