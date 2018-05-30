2178c3d54199311c5ed449d11ed3eb0938f1e49c
caofengbin <1050430934@qq.com>
2018-05-30 20:07:04 +0800

    try to implement MinimumSizeSubarraySum

--- src/com/algorithms/leetcode/array/MinimumSizeSubarraySum.java
+++ src/com/algorithms/leetcode/array/MinimumSizeSubarraySum.java
@@ -17,9 +17,32 @@ package com.algorithms.leetcode.array;
 public class MinimumSizeSubarraySum {
 
     public int minSubArrayLen(int s, int[] nums) {
-        return 0;
+        // 采用滑动窗口法，[leftWindow,rightWindow]左闭右闭区间上，均为滑动窗口的取值范围
+        int leftWindow = 0;
+        int rightWindow = 0;
+        int result = nums.length + 1;
+        int sum = nums[rightWindow];
+        while (rightWindow <= nums.length - 1 && leftWindow <= rightWindow) {
+            if (sum < s && rightWindow + 1 <= nums.length - 1) {
+                // 滑动窗口中的元素和小于目标值
+                rightWindow++;
+                sum += nums[rightWindow];
+            } else {
+                // 滑动窗口中的元素和大于等于目标值
+                result = Math.min(result, rightWindow - leftWindow + 1);
+                sum -= nums[leftWindow];
+                leftWindow++;
+            }
+        }
+
+        if (result == nums.length + 1) {
+            return 0;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        System.out.println(new MinimumSizeSubarraySum().minSubArrayLen(7, nums));
     }
 }
