e25a949bb0bbcc7f2d1dd97ce4fed703152092e4
caofengbin <1050430934@qq.com>
2018-05-30 14:07:17 +0800

    add MinimumSizeSubarraySum

---
+++ src/com/algorithms/leetcode/array/MinimumSizeSubarraySum.java
@@ -0,0 +1,25 @@
+package com.algorithms.leetcode.array;
+
+/**
+ * leetcode 209 links:https://leetcode.com/problems/minimum-size-subarray-sum/description/
+ * <p>
+ * description:
+ * Given an array of n positive integers and a positive integer s,
+ * find the minimal length of a contiguous subarray of which the sum ≥ s.
+ * If there isn't one, return 0 instead.
+ * <p>
+ * Example:
+ * <p>
+ * Input: s = 7, nums = [2,3,1,2,4,3]
+ * Output: 2
+ * Explanation: the subarray [4,3] has the minimal le
+ */
+public class MinimumSizeSubarraySum {
+
+    public int minSubArrayLen(int s, int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+    }
+}
